197b16e41393fc9f15e27a792a9f3a4a42a2ef70
luizadrian1 <106272343+luizadrian1@users.noreply.github.com>
2024-04-16 22:40:29 -0300

    23423444

---
+++ offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1713317771,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/chamada-sheet0.webp",
+		"images/webcam-sheet0.webp",
+		"images/webcam-sheet1.webp",
+		"images/microfone-sheet0.webp",
+		"images/microfone-sheet1.webp",
+		"images/verliveconteiner-sheet0.webp",
+		"images/botaoverlive-sheet0.webp",
+		"images/conteiner1-sheet0.webp",
+		"images/loading-sheet0.webp",
+		"images/loading-sheet1.webp",
+		"images/qrcode_img-sheet0.webp",
+		"images/copiar_carteira-sheet0.webp",
+		"icon-16.webp",
+		"icon-32.webp",
+		"icon-114.webp",
+		"icon-128.webp",
+		"icon-256.webp",
+		"loading-logo.webp",
+		"adapter.js"
+	]
+}
